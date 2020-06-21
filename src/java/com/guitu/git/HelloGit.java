f6ba1e8d3ff2d00ec98d0dff1aeb70ddd81c0a5e
zhangming <1327979032@qq.com>
2020-06-21 16:33:22 +0800

    添加分支

--- src/java/com/guitu/git/HelloGit.java
+++ src/java/com/guitu/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HelloGit.main");
         System.out.println("更新1");
+        System.out.println("分支添加");
     }
 }
