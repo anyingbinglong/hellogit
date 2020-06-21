0d2dc802d270914c19f10ead249ad7a43396027c
zhangming <1327979032@qq.com>
2020-06-21 16:00:36 +0800

    更新1

---
+++ src/java/com/guitu/git/HelloGit.java
@@ -0,0 +1,8 @@
+package com.guitu.git;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit.main");
+        System.out.println("更新1");
+    }
+}
